0384ef0241b57b3a35702ca865f423cbda2a97e7
Ken-Yuan, Kan <410122037@gms.ndhu.edu.tw>
2023-08-12 14:13:08 +0800

    修改套件名稱 vo -> entity，coreDAO

---
+++ src/main/java/core/dao/CoreDao.java
@@ -0,0 +1,25 @@
+package core.dao;
+
+import java.util.List;
+
+import org.hibernate.Session;
+
+import core.util.HibernateUtil;
+
+public interface CoreDao<P, I> {
+	int insert(P pojo);
+
+	int deleteById(I id);
+
+	int update(P pojo);
+
+	P selectById(I id);
+
+	List<P> selectAll();
+	
+	default Session getSession() {
+		return HibernateUtil.getSessionFactory().getCurrentSession();
+//		return HibernateUtil.getSessionFactory().openSession();
+	}
+	
+}
