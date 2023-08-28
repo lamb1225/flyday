554806639ecb3453a5814454b263278c937a8408
Wei <huangweiwu23@gmail.com>
2023-08-28 18:49:21 +0800

    會員行程dao跟entity

---
+++ src/main/java/web/mem/pkg/dao/MemPkgCoupDao.java
@@ -0,0 +1,8 @@
+package web.mem.pkg.dao;
+
+import core.dao.CoreDao;
+import web.mem.pkg.entity.MemPkgCoup;
+
+public interface MemPkgCoupDao extends CoreDao<MemPkgCoup, Integer>{
+
+}
---
+++ src/main/java/web/mem/pkg/dao/impl/MemPkgCoupDaoImpl.java
@@ -0,0 +1,59 @@
+package web.mem.pkg.dao.impl;
+
+import java.util.List;
+
+import javax.persistence.PersistenceContext;
+
+import org.hibernate.Session;
+
+import org.hibernate.query.Query;
+
+import web.mem.pkg.dao.MemPkgCoupDao;
+import web.mem.pkg.entity.MemPkgCoup;
+
+public class MemPkgCoupDaoImpl implements MemPkgCoupDao{
+	@PersistenceContext
+	private Session session;
+
+	@Override
+	public int insert(MemPkgCoup memPkgCoup) {
+		session.persist(memPkgCoup);
+		return 1;
+	}
+
+	@Override
+	public int deleteById(Integer id) {
+		MemPkgCoup	memPkgCoup = session.get(MemPkgCoup.class, id);
+		session.remove(memPkgCoup);
+		return id;
+	}
+
+	@Override
+	public int update(MemPkgCoup memPkgCoup) {
+		final String hql = "UPDATE MemPkgCoup SET memPkgCoupNo=:memPkgCoupNo"
+				+"pkgCoupNo=:pkgCoupNo, memNo=:memNo"
+				+"memPkgCoupState=:memPkgCoupState";
+		
+		Query<?> query = session.createQuery(hql);
+		
+		return query.setParameter("memPkgCoupNo", memPkgCoup.getMemPkgCoupNo())
+				.setParameter("pkgCoupNo", memPkgCoup.getMemPkgCoupNo())
+				.setParameter("memNo", memPkgCoup.getMemNo())
+				.setParameter("memPkgCoupState", memPkgCoup.getMemPkgCoupNo())
+				.executeUpdate();
+	};
+
+	@Override
+	public MemPkgCoup selectById(Integer id) {
+		return session.get(MemPkgCoup.class, id);
+	}
+
+	@Override
+	public List<MemPkgCoup> selectAll() {
+		final String hql= "FROM memPkgCoup ORDER BY memPkgCoupNo";
+		return session.createQuery(hql, MemPkgCoup.class).getResultList();
+	}
+
+	
+	
+}
