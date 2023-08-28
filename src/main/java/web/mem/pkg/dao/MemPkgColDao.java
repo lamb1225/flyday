5e512ad2891385762dfa68790003f9de7e9004ae
Wei <huangweiwu23@gmail.com>
2023-08-28 18:47:48 +0800

    會員行程dao

---
+++ src/main/java/web/mem/pkg/dao/MemPkgColDao.java
@@ -0,0 +1,9 @@
+package web.mem.pkg.dao;
+
+import core.dao.CoreDao;
+import web.mem.pkg.entity.MemPkgCol;
+import web.mem.pkg.entity.MemPkgColId;
+
+public interface MemPkgColDao extends CoreDao<MemPkgCol, MemPkgColId> {
+
+}
