e35d206dfdee18a157105fc88d8977ece28a526c
HiddenMW <51083911+CAI-HaoCheng@users.noreply.github.com>
2023-09-08 23:17:51 +0800

    忘記丟的檔案XD

---
+++ src/main/java/web/act/service/impl/EcPayServiceImpl.java
@@ -0,0 +1,27 @@
+package web.act.service.impl;
+
+import ecpay.payment.integration.AllInOne;
+import ecpay.payment.integration.domain.AioCheckOutALL;
+import web.act.service.EcPayService;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.UUID;
+
+
+public class EcPayServiceImpl implements EcPayService {
+    public String ecpayCheckout(AioCheckOutALL aioCheckOutALL, String returnURL) {
+        Date tradeDate = new Date();
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+        String tracdeDatestr = sdf.format(tradeDate);
+
+        String uuid = UUID.randomUUID().toString().replace("-", "").substring(0, 20);
+        AllInOne all = new AllInOne("");
+        aioCheckOutALL.setMerchantTradeNo(uuid);
+        aioCheckOutALL.setMerchantTradeDate(tracdeDatestr);
+        aioCheckOutALL.setReturnURL(returnURL);
+        aioCheckOutALL.setNeedExtraPaidInfo("N");
+        String form = all.aioCheckOut(aioCheckOutALL, null);
+        return form;
+    }
+}
