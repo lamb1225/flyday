c75418bbc059287c464981a38443a880532b96c5
Wei <huangweiwu23@gmail.com>
2023-09-15 17:28:40 +0800

    0915瑋最新版優惠券管理

--- src/main/webapp/back_end/myassets/js/addcoup.js
+++ src/main/webapp/back_end/myassets/js/addcoup.js
@@ -10,22 +10,62 @@
             	const minicharge = document.querySelector('#minicharge');
             	const state = document.querySelector('#state');
             	const addmsg = document.querySelector('#addmsg');
-
+            	const errmsg = document.querySelector('#errmsg');
+				const moneyRegex = /^[0-9]*$/;
+				var dtToday=new Date();
+				
             	btn.addEventListener('click', () => {
-        			console.log("111");
+        			errmsg.textContent = '';
+        			addmsg.textContent = '';
         			console.log(startdate.value);
 					
         			const coupnameLength = coupname.value.length;
         			if (coupnameLength < 2 || coupnameLength > 40) {
-        				addmsg.textContent = '優惠券名稱長度須介於2~40字元';
+        				errmsg.textContent = '優惠券名稱長度須介於2~40字元';
+        				addmsg.textContent = '新增失敗';
         				return;//停止後續的程式執行
         			}
 
-        			
-        			if (discount.value.trim().length===0) {
-        				addmsg.textContent = '折扣金額不可為空';
+        			if (! moneyRegex.test(discount.value)) {
+        				errmsg.textContent = '只能輸入數字';
+        				addmsg.textContent = '新增失敗';
         				return;
         			}
+        			
+        			if (discount.value.trim().length===0) {
+        				errmsg.textContent = '折扣金額不可為空';
+        				addmsg.textContent = '新增失敗';
+        				return;
+        			}
+        			
+        			if (! moneyRegex.test(minicharge.value)) {
+        				errmsg.textContent = '只能輸入數字';
+        				addmsg.textContent = '新增失敗';
+        				return;
+        			}
+        			
+        			if (minicharge.value.trim().length===0) {
+        				errmsg.textContent = '最低消費金額不可為空';
+        				addmsg.textContent = '新增失敗';
+        				return;
+        			}
+        			       			
+        			const startdateValue = new Date(startdate.value);
+        			startdateValue.setHours(23);
+					startdateValue.setMinutes(59);
+					console.log(dtToday);
+					console.log(startdateValue);
+					
+					
+					
+					
+        			if (startdateValue<dtToday) {
+        				errmsg.textContent = '日期不可小於今天';
+        				addmsg.textContent = '新增失敗';
+        				return;
+        			}
+        			
+        			
         			        			
             		fetch('/flyday/pkg/PkgCoup', {
             			method: 'POST',
@@ -48,11 +88,13 @@
             		}).then(body => {
         				const { successful, message} = body;
         				if (successful) {
-        					console.log("ya");
-        					addmsg.textContent = message;
+        					addmsg.textContent = "新增成功";
+        					setTimeout(function() {        					
+        					location.reload(); 
+        					}, 600);
+        					
         				} else {
-        					console.log("no");
-        					addmsg.textContent = message;
+        					addmsg.textContent = "新增失敗";
         				}
         			});
             			
